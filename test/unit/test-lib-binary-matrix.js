6e55befb035d852344cb581002935cd26d70a522
Thomas Kercheval <spacekattpoispin@gmail.com>
2016-07-08 19:19:21 -0700

    Fix require statement in BinaryMatrix, add tests

--- test/unit/test-lib-binary-matrix.js
+++ test/unit/test-lib-binary-matrix.js
@@ -17,6 +17,38 @@ exports.binary_matrix_creation_test = function (done) {
       var matrix = new BinaryMatrix()
       assert.isOk(typeof matrix === 'object')
     })
+
+    it('Create new Matrix from 2-D Array', function () {
+      var matrix = BinaryMatrix.newOne(elements)
+      expect(matrix.elements).to.deep.equal(elements)
+    })
+
+    it('Zero Matrix created properly', function () {
+      var n = 3
+      var zeroMatrix = BinaryMatrix.zero(n)
+      var expectedElements = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+      expect(zeroMatrix.elements).to.deep.equal(expectedElements)
+    })
+
+    it('Ones Matrix created properly', function () {
+      var n = 3
+      var onesMatrix = BinaryMatrix.one(n)
+      var expectedElements = [[1, 1, 1],
+          [1, 1, 1],
+          [1, 1, 1]]
+      expect(onesMatrix.elements).to.deep.equal(expectedElements)
+    })
+
+    it('Identity Matrix created properly', function () {
+      var n = 3
+      var identityMatrix = BinaryMatrix.iD(n)
+      var expectedElements = [[1, 0, 0],
+          [0, 1, 0],
+          [0, 0, 1]]
+      expect(identityMatrix.elements).to.deep.equal(expectedElements)
+    })
   })
   done()
 }
